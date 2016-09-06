95c58d98d206104751da5706d4392ba8201597c7
Yash Patel <yashpatel26@gmail.com>
2016-09-06 23:58:59 +0530

    Almost done app with app icon

--- Unit_Converter/MainViewController.swift
+++ Unit_Converter/MainViewController.swift
@@ -1,18 +1,30 @@
-//
-//  MainViewController.swift
-//  Unit_Converter
-//
-//  Created by Yash Patel on 06/09/16.
-//  Copyright © 2016 Yash Patel. All rights reserved.
-//
-
 import UIKit
 
 class MainViewController: UIViewController {
+    
+    
+    
+    @IBOutlet var FromSegment: UISegmentedControl!
+    @IBOutlet var ToSegment: UISegmentedControl!
+    @IBOutlet var MainLabel: UILabel!
+    @IBOutlet var TextField: UITextField!
+    
+    // Declaring Variable
+    var FromSegmentIndexOne = String()
+    var FromSegmentIndexTwo = String()
+    
+    var ToSegmentIndexOne = String()
+    var ToSegmentIndexTwo = String()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        FromSegment.setTitle(FromSegmentIndexOne, forSegmentAtIndex: 0)
+        FromSegment.setTitle(FromSegmentIndexTwo, forSegmentAtIndex: 1)
+        
+        ToSegment.setTitle(ToSegmentIndexOne, forSegmentAtIndex: 0)
+        ToSegment.setTitle(ToSegmentIndexTwo, forSegmentAtIndex: 1)
+        
         // Do any additional setup after loading the view.
     }
 
@@ -21,15 +33,113 @@ class MainViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    
+    @IBAction func Calculate(sender: UIButton) {
+        
+        
+        if TextField.text == "" {
+            MainLabel.text = "Atleast enter a value"
+        }
+        
+        else {
+            
+            if isdouble(Arrayofstrings: TextField.text!){
+                
+                let answer =  performCalculation(From: FromSegment.titleForSegmentAtIndex(FromSegment.selectedSegmentIndex)!, To: ToSegment.titleForSegmentAtIndex(ToSegment.selectedSegmentIndex)!, TextField: Double(TextField.text!)!)
+                
+                
+                MainLabel.text = answer
+                
+            }
+            
+            else {
+                
+                MainLabel.text = "Enter Valid Number"
+                
+            }
+            
+        }
+        
     }
-    */
-
+    
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+    
 }
+
+func isdouble(Arrayofstrings a:String ...) -> Bool {
+    print (a)
+    var isdub = true
+    for i in a {
+        
+        let doubcheck:Double? = Double(i)
+        if doubcheck == nil{
+            isdub = false
+        }
+        
+    }
+    print (isdub)
+    return isdub
+}
+
+func performCalculation(From f:String , To t:String , TextField num:Double) -> String {
+    
+    if f == t {
+        return "Dont Be Dumb :("
+    }
+    
+    else if f == "Dollar" && t == "Indian Rupee" {
+        
+        let answer = "\(round(num * 66.30*100)/100) \(t)"
+        return answer
+    }
+        
+    else if f == "Indian Rupee" && t == "Dollar" {
+        
+        let answer = "\(round(num * 0.02*100)/100) \(t)"
+        return answer
+        
+    }
+        
+    else if f == "Meter" && t == "Centimeter" {
+        
+        let answer = "\(round(num * 100*100)/100) \(t)"
+        return answer
+        
+    }
+        
+    else if f == "Centimeter" && t == "Meter" {
+        
+        let answer = "\(round(num * 0.01*100)/100) \(t)"
+        return answer
+        
+    }
+        
+    else if f == "Celsius" && t == "Fahrenheit" {
+        
+        let fahrenheit = round(((num * (9/5)) + 32)*100)/100
+        let answer = "\(fahrenheit) \(t)"
+        return answer
+        
+    }
+        
+    else if f == "Fahrenheit" && t == "Celsius" {
+        
+        let celsius = round(((num - 32)*(5/9))*100)/100
+        let answer = "\(celsius) \(t)"
+        return answer
+        
+    }
+    
+    else {
+        
+        return "Boooo"
+    }
+    
+}
+
+
+
+
+
--- Unit_Converter/ViewController.swift
+++ Unit_Converter/ViewController.swift
@@ -1,15 +1,11 @@
-//
-//  ViewController.swift
-//  Unit_Converter
-//
-//  Created by Yash Patel on 06/09/16.
-//  Copyright © 2016 Yash Patel. All rights reserved.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet var TypeSegment: UISegmentedControl!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +16,41 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        let MainDestinationViewController : MainViewController = segue.destinationViewController as! MainViewController
+        
+        let segment = TypeSegment.selectedSegmentIndex
+        
+        switch segment {
+            
+        case 0 :
+            
+            MainDestinationViewController.FromSegmentIndexOne = "Indian Rupee"
+            MainDestinationViewController.FromSegmentIndexTwo = "Dollar"
+            MainDestinationViewController.ToSegmentIndexOne = "Dollar"
+            MainDestinationViewController.ToSegmentIndexTwo = "Indian Rupee"
+            
+        case 1 :
+            
+            MainDestinationViewController.FromSegmentIndexOne = "Celsius"
+            MainDestinationViewController.FromSegmentIndexTwo = "Fahrenheit"
+            MainDestinationViewController.ToSegmentIndexOne = "Fahrenheit"
+            MainDestinationViewController.ToSegmentIndexTwo = "Celsius"
+            
+        case 2 :
+            
+            MainDestinationViewController.FromSegmentIndexOne = "Meter"
+            MainDestinationViewController.FromSegmentIndexTwo = "Centimeter"
+            MainDestinationViewController.ToSegmentIndexOne = "Centimeter"
+            MainDestinationViewController.ToSegmentIndexTwo = "Meter"
+            
+        default:
+            print ("Error @ preparing segue")
+            
+            
+        }
+    }
 
 }
 
